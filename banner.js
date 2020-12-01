daae9b2fec8f58d48949e7c05588f5a0d7aced70
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-12-01 18:17:40 +0900

    .travis.yml 수정 (repo 이름이 잘못됨)

--- banner.js
+++ banner.js
@@ -1,10 +1,8 @@
-// const childProcess = require("child_process")
+const childProcess = require("child_process");
 
 module.exports = function banner() {
-  // const commit = childProcess.execSync("git rev-parse --short HEAD")
-  // const user = childProcess.execSync("git config user.name")
-  const commit = "git commit version";
-  const user = "git user";
+  const commit = childProcess.execSync("git rev-parse --short HEAD");
+  const user = childProcess.execSync("git config user.name");
   const date = new Date().toLocaleString();
 
   return (
