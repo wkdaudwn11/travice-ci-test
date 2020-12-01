094474a59840a7cd47db9f6fc40118d8bb4567d1
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-12-01 17:31:24 +0900

    배포세팅 및 배포테스트

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# Build environment
+FROM node:12.8.0 as builder
+WORKDIR /app
+ENV PATH /app/node_modules/.bin:$PATH
+COPY package.json /app/package.json 
+RUN npm install --silent
+RUN npm install react-scripts@3.4.0 -g --silent
+RUN rm -r node_modules/terser
+RUN npm install terser@3.14.1 --save-dev
+COPY . .
+RUN npm run build
+
+# Temp Build environment
+# FROM node:12.8.0 as builder
+# WORKDIR /app
+# COPY ./build /app/build
+
+# Production environment
+FROM nginx:1.16.0-alpine
+COPY --from=builder /app/dist /usr/share/nginx/html
+RUN rm -rf /etc/nginx/conf.d
+COPY conf /etc/nginx
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker-compose up -d --build > deploy_result
+
+if [ ! 0 = $? ]; then
+        message=`cat deploy_result`
+        curl --data "travis-ci-test 배포에 실패하였습니다.  $message" 'https://d-dive.slack.com/services/hooks/slackbot?token=7WOMm81rpCFOhNGNCt46ctS5&channel=%23mj-test'
+else
+        curl --data "travis-ci-test 배포에 성공하였습니다." 'https://d-dive.slack.com/services/hooks/slackbot?token=7WOMm81rpCFOhNGNCt46ctS5&channel=%23mj-test'
+fi
+
+# docker rmi $(docker images -f "dangling=true" -q)
+
+# echo "remote volumn"
+docker volume rm $(docker volume ls -qf dangling=true)
+
+# echo "remote docker images"
+docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
+docker images -a | sed '1 d' | awk '{print $3}' | xargs -L1 docker rmi -f
