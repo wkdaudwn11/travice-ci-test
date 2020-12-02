055aec4b9efe8f75975c84d35cb38fa05741db91
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-12-02 18:29:31 +0900

    execute-deploy.sh 코드 수정

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
 curl --data "travis-ci-test execute-deploy.sh 실행함." 'https://d-dive.slack.com/services/hooks/slackbot?token=7WOMm81rpCFOhNGNCt46ctS5&channel=%23mj-test'
+
 cd /home/ec2-user/deploy/travice-ci-test
-./deploy.sh > /dev/null 2> /dev/null < /dev/null &
+
+curl --data "travis-ci-test travice-ci-test 폴더로 이동함." 'https://d-dive.slack.com/services/hooks/slackbot?token=7WOMm81rpCFOhNGNCt46ctS5&channel=%23mj-test'
+# ./deploy.sh > /dev/null 2> /dev/null < /dev/null &
+
+docker-compose up -d --build > deploy_result
+
+if [ ! 0 = $? ]; then
+        message=`cat deploy_result`
+        curl --data "travis-ci-test 배포에 실패하였습니다.  $message" 'https://d-dive.slack.com/services/hooks/slackbot?token=7WOMm81rpCFOhNGNCt46ctS5&channel=%23mj-test'
+else
+        curl --data "travis-ci-test 배포에 성공하였습니다!" 'https://d-dive.slack.com/services/hooks/slackbot?token=7WOMm81rpCFOhNGNCt46ctS5&channel=%23mj-test'
+fi
+
+# docker rmi $(docker images -f "dangling=true" -q)
+
+# echo "remote volumn"
+docker volume rm $(docker volume ls -qf dangling=true)
+
+# echo "remote docker images"
+docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
+docker images -a | sed '1 d' | awk '{print $3}' | xargs -L1 docker rmi -f
